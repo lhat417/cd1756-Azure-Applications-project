270e7372b6ee88d01de8f22ba366743b20564a8d
lhat417 <laurenhatfield@chevron.com>
2025-03-05 21:43:25 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.WARNING)​
+streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
